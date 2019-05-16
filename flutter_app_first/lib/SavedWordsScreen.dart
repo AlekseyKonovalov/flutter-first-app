a990e1a05e0f33555c9264f663d43a4a2f58b2f5
Aleksey K <aleksey.konovalov17@gmail.com>
2019-05-16 17:15:42 +0500

    Update SavedWordsScreen.dart

--- flutter_app_first/lib/SavedWordsScreen.dart
+++ flutter_app_first/lib/SavedWordsScreen.dart
@@ -2,18 +2,13 @@
 import 'package:flutter/material.dart';
 
 class SavedWordsScreen extends StatefulWidget {
-
   SavedWordsScreen(this._divided);
-
   final List<Widget> _divided;
-
   @override
   State<StatefulWidget> createState() => _SavedWordsScreenState();
-
 }
 
 class _SavedWordsScreenState extends State<SavedWordsScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,5 +18,4 @@ class _SavedWordsScreenState extends State<SavedWordsScreen> {
       body: ListView(children: widget._divided),
     );
   }
-
 }
