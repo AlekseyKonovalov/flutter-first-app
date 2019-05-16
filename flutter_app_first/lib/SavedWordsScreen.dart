b90939a0ae5e654a1e2607833479f2bee4c3efa2
Aleksey K <aleksey.konovalov17@gmail.com>
2019-05-16 16:07:02 +0500

    added RandomWordsScreen

--- flutter_app_first/lib/SavedWordsScreen.dart
+++ flutter_app_first/lib/SavedWordsScreen.dart
@@ -1,18 +1,19 @@
-import 'package:english_words/english_words.dart';
+
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-
 
 class SavedWordsScreen extends StatefulWidget {
+
   SavedWordsScreen(this._divided);
+
   final List<Widget> _divided;
 
   @override
   State<StatefulWidget> createState() => _SavedWordsScreenState();
+
 }
 
 class _SavedWordsScreenState extends State<SavedWordsScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,4 +23,5 @@ class _SavedWordsScreenState extends State<SavedWordsScreen> {
       body: ListView(children: widget._divided),
     );
   }
+
 }
